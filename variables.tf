181634bc7fc33f75fd01acd7caac0a947b0029ba
sentinelman88 <72362145+sentinelman88@users.noreply.github.com>
2020-10-08 15:02:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,29 @@
 variable "organization_id" {
-  description = "The organization id for putting the policy"
   type        = string
-  default     = null
+  description = "The organization id"
+}
+
+variable "gcp_auth_file" {
+  type = string
+  description = "GCP authentication file"
+}
+
+variable "app_project" {
+  type = string
+  description = "GCP project name"
+}
+
+variable "app_name" {
+  type = string
+  description = "Application name"
+}
+
+variable "app_domain" {
+  type = string
+  description = "Application domain"
+}
+
+variable "app_environment" {
+  type = string
+  description = "Application environment"
 }
