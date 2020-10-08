2a83ef4ac8cb34341f33b53a3a20c3de1cb7b157
sentinelman88 <72362145+sentinelman88@users.noreply.github.com>
2020-10-08 18:41:16 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,9 @@
-variable "organization_id" {
+variable "project_id" {
   type        = string
-  description = "The organization id"
+  description = "The project id"
 }
 
 variable "gcp_auth_file" {
   type = string
   description = "GCP authentication file"
-}
-
-variable "app_project" {
-  type = string
-  description = "GCP project name"
-}
-
-variable "app_name" {
-  type = string
-  description = "Application name"
-}
-
-variable "app_domain" {
-  type = string
-  description = "Application domain"
-}
-
-variable "app_environment" {
-  type = string
-  description = "Application environment"
 }
