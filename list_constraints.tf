a503c1d54d4633b365179a4b4ed50d64509ec72b
sentinelman88 <72362145+sentinelman88@users.noreply.github.com>
2020-10-08 18:17:33 -0400

    Update and rename list_constraints.tf to project_list_constraints.tf

--- list_constraints.tf
+++
@@ -1,8 +0,0 @@
-resource "google_organization_policy" "services_policy" {
-  org_id     = var.organization_id
-  constraint = "serviceuser.services"
-
-  restore_policy {
-    default = true
-  }
-}
