9032d230ee7fab1634af17575e3244710f84c39b
sentinelman88 <72362145+sentinelman88@users.noreply.github.com>
2020-10-08 18:35:39 -0400

    Update and rename boolean_constraints.tf to project_boolean_constraints.tf

--- boolean_constraints.tf
+++
@@ -1,8 +0,0 @@
-resource "google_organization_policy" "serial_port_policy" {
-  org_id     = var.organization_id
-  constraint = "compute.disableSerialPortAccess"
-
-  boolean_policy {
-    enforced = true
-  }
-}
