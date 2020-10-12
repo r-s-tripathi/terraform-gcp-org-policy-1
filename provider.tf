7518b758038d4d4afd08b204fc9c64cdd14132e8
sentinelman88 <72362145+sentinelman88@users.noreply.github.com>
2020-10-12 16:42:44 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.12"
 }
 
 provider "google" {
-  project = var.app_project
+  project = var.project_id
   credentials = file("credentials_file.json")
   region  = var.gcp_region_1
   zone    = var.gcp_zone_1
